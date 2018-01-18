d371d19312882772a44950cafe4e04fed224e405
Rinsho <Rinsho@users.noreply.github.com>
2018-01-17 19:17:11 -0600

    Accessor update
    Modified PropertyInfo to use a delegate. Removed some YAGNI code.
    Included OverflowException catch in RegexContainer.

--- RegexAttributeUtility/Wrappers/FieldInfoWrapper.cs
+++ RegexAttributeUtility/Wrappers/FieldInfoWrapper.cs
@@ -6,16 +6,9 @@ namespace RegularExpression.Utility.Data
     internal class FieldInfoWrapper : IDataAccessor
     {
         private FieldInfo _field;
-        public Type DataType { get; }
 
-        public FieldInfoWrapper(FieldInfo field)
-        {
+        public FieldInfoWrapper(FieldInfo field) =>
             _field = field;
-            DataType = _field.FieldType;
-        }
-
-        public object GetValue(object container) =>
-            _field.GetValue(container);
 
         public void SetValue(object container, object value) =>
             _field.SetValue(container, value);
